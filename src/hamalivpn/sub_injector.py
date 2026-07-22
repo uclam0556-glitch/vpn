8ad64c158206313f79392da0b85cd200f32a9f25
uclam0556-glitch <uclam0556@gmail.com>
2026-07-22 14:21:24 +0300

    chore: remove hacky germany node fallback

--- src/hamalivpn/sub_injector.py
+++ src/hamalivpn/sub_injector.py
@@ -982,20 +982,6 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
                                     if "Автовыбор" not in dec_link:
                                         other_links.append(link)
 
-                            if not any("206.245.134.58" in link for link in premium_nl + premium_fr + premium_uk + premium_fi + reserve_links + other_links):
-                                if uuid and pbk and sni and sid:
-                                    reserve_links.append(
-                                        reality_share_link(
-                                            uuid,
-                                            "206.245.134.58",
-                                            443,
-                                            sni,
-                                            pbk,
-                                            sid,
-                                            happ_label("🇩🇪 Германия (Test)", "VLESS | TCP | Reality | JSON")
-                                        )
-                                    )
-
                             cluster_json = """{
   "remarks": "🇪🇺 Автовыбор",
   "burstObservatory": {
